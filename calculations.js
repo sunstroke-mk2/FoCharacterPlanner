75bace25ef7d89fac7c7be70fb1e1b743aaa8807
sunstroke-mk2 <55920159+sunstroke-mk2@users.noreply.github.com>
2022-12-30 14:41:09 +0300

    Update calculations.js

--- calculations.js
+++ calculations.js
@@ -1,7 +1,8 @@
+
 let CharacterStats=
 {
-  'RunTime' :              200,
   'MaxLife' :              100,
+  'RunTime' :              200,
   'HpRegenVal' :           20,
   'HpRegenTime' :          30,
   'ActionPoints' :         100,
@@ -10,34 +11,39 @@ let CharacterStats=
   'LookDistance' :         35,
   'ArmorClass' :           0,
   'CarryWeight' :          57,
+ // 'CriticalChance' :       10,
+
   'MeleeDamage' :          20,
-  'CriticalChanceCC' :     0,
-  'CriticalChance' :       10,
-  'CriticalDamageCC' :     0,
   'ToHitCC' :              0,
+  'CriticalChanceCC' :     0,
+  'CriticalDamageCC' :     0,
+  'BonusMeeleDamage' :    0,
+ 
   'RangedDamage' :         0,
-  'CriticalChanceRC' :     0,
-  'BonusMeeleDamage' :    0, 
-  'BonusRangedDamage' :   0,
-  'CriticalDamageRC' :     0,
   'ToHitRC' :              0,
-  'SkillGain' :            0,
-  'BonusExp' :             0,
+  'CriticalChanceRC' :     0,
+  'CriticalDamageRC' :     0,
+  'BonusRangedDamage' :    0,
+  'DamageMultiplier' :     100,
+  'CritDamageMultiplier' : 100,
+  'EnemyDamageMultiplier': 100,
   'ActivePerksDuration' :  0,
   'ActivePerksCooldown' :  0,
   'TimeoutTimeFirstAid' :  60,
-  'DamageMultiplier' :      100,
-  'CritDamageMultiplier' :  100,
-  'EnemyDamageMultiplier': 100,
+  'HealingBonus' :          0,
+
+ 
   'ApBonusAttack' :          0,
   'ApBonusUse' :              0,
-  'HealingBonus' :            0,
+
   'Sneak' :                  10,
   'UtilityTimeout':           0,
   'DrugLength':               0,
   'CriticalResistance':       0,
   'PoisonResistance':          0,
-  'RadiationResistance':      0
+  'RadiationResistance':      0,
+  'SkillGain' :            0,
+  'BonusExp' :             0,
 }
 let Perks =
 [
@@ -107,8 +113,8 @@ let PerksValues =
 }
 let Traits =
 [
-   'Armorpadding',
-   'BiggerBooms',
+  'Armorpadding',
+  'BiggerBooms',
   'Bloodthirsty',
   'Bloodymess',
   'Bonehead',
@@ -140,7 +146,6 @@ let Traits =
   'Tank',
   'TeamPlayer',
   'Tribal'
-  
 ]
 let TraitsValues =
 {
@@ -219,6 +224,7 @@ function Initialization()
     {
       let s='';
       s="<div class='perk' name="+Perks[a]+" id=p"+a+">"+Perks[a]+"<span class='somenumbers'></span></div><span></span>";
+      //s.addClass('itemHighlighted');
       $('#outputPerks').append(s);
     }
   
@@ -271,7 +277,8 @@ $('.perk').hover
           TotalPerksCount+=1;
           let s='#p'+a;
           $(s).children('span').text( TakenPerksCount[a]);
-          $(s).css("color","red");
+         // $(s).css("color","red");
+          $(s).addClass("itemHighlighted");
        }
       $('#perkscounter').text(TotalPerksCount+'/'+MAXPERKS);
      calculateStats();
@@ -288,15 +295,16 @@ $('.perk').hover
           TotalPerksCount-=1;
           let s='#p'+a;
           let tx="";
-          let cl="black";
+          let cl="item";
           if(TakenPerksCount[a]>0)
            {
-              cl="red";
+              cl="itemHighlighted";
               tx=TakenPerksCount[a];
            }
-          
+          $(s).removeClass();
           $(s).children('span').text( tx);
-          $(s).css("color",cl);
+         // $(s).css("color",cl);
+         $(s).addClass(cl);
        }
       $('#perkscounter').text(TotalPerksCount+'/'+MAXPERKS);
      calculateStats();
@@ -314,14 +322,16 @@ $(".toggler").mousedown
               {
                 TakenTraitsCount[i]+=1;
                 TotalTraitsCount+=1;
-                $('#t'+i).css("color","red");
+               // $('#t'+i).css("color","red");
+                 $('#t'+i).toggleClass('itemHighlighted item')
               }
           }
         else
           {
             TakenTraitsCount[i]-=1;
             TotalTraitsCount-=1;
-            $('#t'+i).css("color","black");
+            //$('#t'+i).css("color","black");
+            $('#t'+i).toggleClass('item itemHighlighted')
           }
         $('#traitscounter').text(TotalTraitsCount+"/"+MAXTRAITS);
         calculateStats();
@@ -341,12 +351,14 @@ $(".drug").mousedown
      i=Drugs.indexOf(a);
      if(TakenDrugsCount[i]==0)
        {
-          $('#d'+i).css("color","red");
+         $('#d'+i).toggleClass('itemHighlighted item')
+          //$('#d'+i).css("color","red");
           TakenDrugsCount[i]=1;
        }
     else
       {
-        $('#d'+i).css("color","black");
+        $('#d'+i).toggleClass('item itemHighlighted')
+       // $('#d'+i).css("color","black");
         TakenDrugsCount[i]=0;
       }
     
@@ -361,39 +373,51 @@ function calculateStats()
   let g =0;  if(TakenTraitsCount[Traits.indexOf('ChemReliant')]>0&&TakenDrugsCount[Drugs.indexOf('Mentats')]==0&&TakenDrugsCount[Drugs.indexOf('Jet')]==0&&TakenDrugsCount[Drugs.indexOf('Psycho')]==0&&TakenDrugsCount[Drugs.indexOf('Buffout')]==0)
   {g=getTraitBonus('ChemReliant','ApReduce');}
   $('#ActionPoints').children('.somenumbers').text(100+getTraitBonus('Tribal','ActionPoints')+getDrugBonus('Jet','ActionPoints')+getDrugBonus('Cookie','ActionPoints')+g);
-  $('#LookDistance').children('.somenumbers').text(35+getTraitBonus('ShortSighted','LookDistance')+getDrugBonus('Psycho','LookDistance')+getPerkBonus('FarSight'));
   
-  $("#CriticalDamageCC").children('.somenumbers').text(getTraitBonus('Bruiser','BonusDamageCC')+getPerkBonus('MeleeCriticalDamage')+getDrugBonus('Mentats','CriticalDamageRC'));
-  $("#CriticalChanceCC").children('.somenumbers').text(0+getTraitBonus('Bruiser','CriticalChanceCC')+getPerkBonus('MeleeCriticalChance'));
+  let baseCC=lowCap((10+getTraitBonus('Finesse','CriticalChance')+getTraitBonus('Bonehead','CriticalChance')+getPerkBonus('MoreCriticals')),0);
+  
+  let lookdis =35+getTraitBonus('ShortSighted','LookDistance')+getDrugBonus('Psycho','LookDistance')+getPerkBonus('FarSight');
+  $('#LookDistance').children('.somenumbers').text(lowCap(lookdis,10));
+  
+  $("#CriticalDamageCC").children('.somenumbers').text((getTraitBonus('Bruiser','BonusDamageCC')+getPerkBonus('MeleeCriticalDamage')+getDrugBonus('Mentats','CriticalDamageRC'))+'%');
+  let ccCC=(0+getTraitBonus('Bruiser','CriticalChanceCC')+getPerkBonus('MeleeCriticalChance'));
+  $("#CriticalChanceCC").children('.somenumbers').text((lowCap(ccCC,0)+baseCC)+'%');
 // $('Rad-X') 
-  $("#ApRegenerationTime").children('.somenumbers').text(60+getTraitBonus('DrunkMaster',"ApRegenerationTime")+getDrugBonus('Cola','ApRegenerationTime')+getDrugBonus('Jet','ApRegenerationTime')+getPerkBonus('ActionBoy','ApRegenerationTime'));
+  let ApR=(60+getTraitBonus('DrunkMaster',"ApRegenerationTime")+getDrugBonus('Cola','ApRegenerationTime')+getDrugBonus('Jet','ApRegenerationTime')+getPerkBonus('ActionBoy','ApRegenerationTime'));
+  $("#ApRegenerationTime").children('.somenumbers').text(ApR);
   
   let a=0;
   if(TakenTraitsCount[Traits.indexOf('DrunkMaster')]>0&&TakenDrugsCount[Drugs.indexOf('Alcohol')]>0)a=getTraitBonus('DrunkMaster','Dodge');
   $("#Dodge").children('.somenumbers').text(a+getPerkBonus('Dodger',));
     
   $("#HpRegenTime").children('.somenumbers').text(60+getTraitBonus('FastMetabolism','HpRegenTime')+getDrugBonus('Food','HpRegenTime')+getPerkBonus('QuickRegeneration'));
-  $('#PoisonResistance').children('.somenumbers').text(0+getTraitBonus('FastMetabolism','PoisonResistance')+getPerkBonus('SnakeEater'));
-  $('#RadiationResistance').children('.somenumbers').text(0+getTraitBonus('FastMetabolism','RadiationResistance')+getDrugBonus('Rad-X','RadiationResistance')+getPerkBonus('RadRoach'));
+  
+  let poires=0+getTraitBonus('FastMetabolism','PoisonResistance')+getPerkBonus('SnakeEater');
+  $('#PoisonResistance').children('.somenumbers').text(lowCap(poires,0)+'%');
+  
+  let radres=0+getTraitBonus('FastMetabolism','RadiationResistance')+getPerkBonus('RadRoach');
+  $('#RadiationResistance').children('.somenumbers').text((getDrugBonus('Rad-X','RadiationResistance')+lowCap(radres,0))+'%');
   
   $('#ApBonusAttack').children('.somenumbers').text(0+getTraitBonus('FastShot','ApBonusAttack')+getPerkBonus('Gunslinger'));
   $('#ApBonusUse').children('.somenumbers').text(getPerkBonus('QuickHands'));
   
-  $('#CriticalChance').children('.somenumbers').text(10+getTraitBonus('Finesse','CriticalChance')+getTraitBonus('Bonehead','CriticalChance')+getPerkBonus('MoreCriticals')),   
-  $('#DamageMultiplier').children('.somenumbers').text(100+getTraitBonus('Kamikaze','DamageMultiplier')+getTraitBonus('OneHander','DamageMultiplier')+getTraitBonus('Pacifist','DamageMultiplier')),
-   $('#CritDamageMultiplier').children('.somenumbers').text(getTraitBonus('Pacifist','CritDamageMultiplier'));
+
+ // $('#CriticalChance').children('.somenumbers').text(lowCap(cc,0)+'%'),   
+  $('#DamageMultiplier').children('.somenumbers').text((100+getTraitBonus('Kamikaze','DamageMultiplier')+getTraitBonus('OneHander','DamageMultiplier')+getTraitBonus('Pacifist','DamageMultiplier'))+'%'),
+  $('#CritDamageMultiplier').children('.somenumbers').text((100+getTraitBonus('Pacifist','CritDamageMultiplier'))+'%');
   
    $('#HealingBonus').children('.somenumbers').text(getTraitBonus('Pacifist','HealingBonus')+getPerkBonus('PainKiller'));
-   $('#TimeoutTimeFirstAid').children('.somenumbers').text(60+getPerkBonus('Medic')),
+   $('#TimeoutTimeFirstAid').children('.somenumbers').text(60+getPerkBonus('Medic'));
+     
+   let hpreg=(20+getTraitBonus('RadChild','HpRegenVal')+getPerkBonus('NaturalHealing'));
+   $('#HpRegenVal').children('.somenumbers').text(lowCap(hpreg,0)),
     
-   $('#HpRegenVal').children('.somenumbers').text(20+getTraitBonus('RadChild','HpRegenVal')+getPerkBonus('NaturalHealing')),
+   $('#CriticalResistance').children('.somenumbers').text((getTraitBonus('SmallFrame','CriticalResistance')+getDrugBonus('Buffout','CriticalResistance')+getPerkBonus('LuckyBastard'))+'%'),
     
-   $('#CriticalResistance').children('.somenumbers').text(getTraitBonus('SmallFrame','CriticalResistance')+getDrugBonus('Buffout','CriticalResistance')+getPerkBonus('LuckyBastard')),
+    $('#ToHitRC').children('.somenumbers').text((getDrugBonus('Cigarettes','ToHitRC')+getPerkBonus('Sharpshooter'))+'%'),
+    $('#ToHitCC').children('.somenumbers').text((getDrugBonus('Cigarettes','ToHitCC')+getPerkBonus('AccuratePunches'))+'%'),
     
-    $('#ToHitRC').children('.somenumbers').text(getDrugBonus('Cigarettes','ToHitRC')+getPerkBonus('Sharpshooter')),
-    $('#ToHitCC').children('.somenumbers').text(getDrugBonus('Cigarettes','ToHitCC')+getPerkBonus('AccuratePunches')),
-    
-    $('#CriticalChanceRC').children('.somenumbers').text(getPerkBonus('Hunter'));
+    $('#CriticalChanceRC').children('.somenumbers').text(((getPerkBonus('Hunter'))+baseCC)+'%');
     
     $('#CriticalDamageRC').children('.somenumbers').text((getDrugBonus('Mentats','CriticalDamageRC')+getPerkBonus('WeakPoint'))+'%');
    // $('#CriticalDamageCC').children('.somenumbers').text(getDrugBonus('Mentats','CriticalDamageRC')+'%');
@@ -401,8 +425,8 @@ function calculateStats()
 
    $('#Sneak').children('.somenumbers').text(10+getPerkBonus('Ghost'));
    $('#UtilityTimeout').children('.somenumbers').text(getPerkBonus('Tinkerer'));
-   $('#BonusMeeleDamage').children('.somenumbers').text(getPerkBonus('CQCTraining'));
-   $('#BonusRangedDamage').children('.somenumbers').text(getPerkBonus('BonusRangedDamage'));
+   $('#BonusMeeleDamage').children('.somenumbers').text(getPerkBonus('CQCTraining')+'%');
+   $('#BonusRangedDamage').children('.somenumbers').text(getPerkBonus('BonusRangedDamage')+'%');
    // $('#UtilityTimeout').children('.somenumbers').text(getPerkBonus('Tinkerer')));
   $('#DrugLength').children('.somenumbers').text(30+getPerkBonus('Junkie')+getTraitBonus('ChemReliant','DrugLength')); 
   //  'SkillGain' :            0,
@@ -446,4 +470,11 @@ function getPerkBonus( perk)
     return 0;
   let a = PerksValues[perk][perksTaken-1];
     return a;
+}
+function lowCap(val,min)
+{
+  if(val<min)
+    return min;
+  else
+    return val;
 }
